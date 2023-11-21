1910f8f5d2e4189c9bf6e03a535027e5959513a2
George Raduta <george.raduta@cern.ch>
2023-11-21 20:54:07 +0100

    Add solutions for introductory NodeJS

--- 0-Introductory-Exercises/index.js
+++ 0-Introductory-Exercises/index.js
@@ -1,1 +1,29 @@
 // Your code goes here
+
+const {HttpServer, WebSocket, WebSocketMessage, Log} = require('@aliceo2/web-ui');
+
+const logger = new Log('workshop-2023');
+
+const http = new HttpServer({
+  port: 8080
+}, {
+  expiration: '60s'
+});
+
+http.addStaticPath('public');
+
+http.get('/hello-world', (_, res) => {
+  res.status(200).json({message: 'Hello World!'});
+}, {public: true});
+
+http.get('/hello-private', (_, res) => {
+  res.status(200).json({message: 'Hi, there'})
+});
+
+const ws = new WebSocket(http);
+
+ws.bind('hello', (message) => {
+  console.log('Server received:', message);
+  logger.info('test')
+  return new WebSocketMessage().setCommand('hello-back').setPayload("hello back");
+});
